b9b4e86518ecd41bb0f73c6661d83151ab792618
AyushBhatt <ayushbhatt1425@gmail.com>
2023-04-02 22:18:58 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './components/nav/Nav'
+import Body from './components/body/Body'
+import Footer from './components/footer/Footer'
+const App = () => {
+  return (
+    <>
+      <Nav/>
+      <Body/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
